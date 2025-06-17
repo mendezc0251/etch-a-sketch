b14f57a01d53a7302d62a8880ed4f46dc5e2b485
mendezc0251 <cmendez0251@gmail.com>
2025-06-17 17:50:42 -0500

    Added button and correct canvas size

--- etch.js
+++ etch.js
@@ -1,4 +1,18 @@
-for(i=1;i<=8100;i++){
+const resizeButton = document.querySelector("button")
+let area = 256
+let perimeter = Math.sqrt(area)
+resizeButton.addEventListener("click", (event)=>{
+    area = Number(prompt("How many pixels would you like on each side of your canvas?(100 MAX)"))**2
+    const divContainer = document.querySelector(".grid-container")
+    divContainer.style.minHeight = `${perimeter}"px"`
+    divContainer.style.maxWidth = `${perimeter}"px"`
+})
+ 
+const divContainer = document.querySelector(".grid-container")
+divContainer.style.height = `$(perimeter)"px"`
+divContainer.style.width = `$(perimeter) "px"`
+
+for(i=1;i<=area;i++){
     const gridDiv = document.createElement("div")
     gridDiv.className = "grid-divs"
     const divContainer = document.querySelector(".grid-container")
