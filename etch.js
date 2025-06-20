e0d8d61ad74ac81aefee7b56a7ad9a98cf86c0bf
mendezc0251 <cmendez0251@gmail.com>
2025-06-20 13:42:39 -0500

    Finished logic on resize

--- etch.js
+++ etch.js
@@ -1,28 +1,41 @@
 const resizeButton = document.querySelector("button")
 let area = 256
 let perimeter = Math.sqrt(area)
-resizeButton.addEventListener("click", (event)=>{
-    area = Number(prompt("How many pixels would you like on each side of your canvas?(100 MAX)"))**2
-    const divContainer = document.querySelector(".grid-container")
-    divContainer.style.minHeight = `${perimeter}"px"`
-    divContainer.style.maxWidth = `${perimeter}"px"`
-})
- 
-const divContainer = document.querySelector(".grid-container")
-divContainer.style.height = `$(perimeter)"px"`
-divContainer.style.width = `$(perimeter) "px"`
+let canvasContainer = document.getElementById("canvas")
+canvasContainer.style.minHeight = `${perimeter}px`
+canvasContainer.style.maxWidth = `${perimeter}px`
+let gridDiv = document.getElementsByClassName("grid-divs")
 
-for(i=1;i<=area;i++){
+resizeButton.addEventListener("click", (event) => {
+    area = Number(prompt("How many pixels would you like on each side of your canvas?(100 MAX)")) ** 2
+    perimeter = Math.sqrt(area)
+    canvasContainer.style.minHeight = `${perimeter}px`
+    canvasContainer.style.maxWidth = `${perimeter}px`
+    canvasContainer.innerHTML = ""
+    for (i = 1; i <= area; i++) {
+        const gridDiv = document.createElement("div")
+        gridDiv.className = "grid-divs"
+        const divContainer = document.querySelector("#canvas")
+        divContainer.appendChild(gridDiv)
+    }
+
+    Array.from(gridDiv).forEach(div => {
+        div.addEventListener("mouseover", (event) => {
+            div.style.backgroundColor = "black"
+        })
+    })
+})
+
+for (i = 1; i <= area; i++) {
     const gridDiv = document.createElement("div")
     gridDiv.className = "grid-divs"
-    const divContainer = document.querySelector(".grid-container")
+    const divContainer = document.querySelector("#canvas")
     divContainer.appendChild(gridDiv)
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    const gridDiv = document.getElementsByClassName("grid-divs")
+document.addEventListener("DOMContentLoaded", function () {
     Array.from(gridDiv).forEach(div => {
-        div.addEventListener("mouseover",(event)=>{
+        div.addEventListener("mouseover", (event) => {
             div.style.backgroundColor = "black"
         })
     })
